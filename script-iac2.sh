e77faa6f363faa98be0ea300e807d6bd5b3af095
RobsonArcoleze <robson.arcoleze29@gmail.com>
2023-03-03 23:48:44 -0300

    fix:Corrigido erro no código

--- script-iac2.sh
+++ script-iac2.sh
@@ -15,6 +15,7 @@ echo "Baixando e copiando os arquivos da aplicação..."
 
 cd/tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
 cd linux-site-dio
 cp -R * /var/www/html
 
